5cc56f42e853592d656a15b336d542fe1290b11b
RogerioBlanco <rogerio.b.nunes@gmail.com>
2017-06-16 18:04:27 +0100

    First initial setup

--- example/src/main/java/org/selenium/example/demoqa/MainExample.java
+++ example/src/main/java/org/selenium/example/demoqa/MainExample.java
@@ -1,5 +1,25 @@
 package org.selenium.example.demoqa;
 
+import org.easylenium.selenium.FactoryMainSelenium;
+import org.easylenium.selenium.settings.Browser;
+import org.easylenium.selenium.settings.SeleniumSettings;
+import org.easylenium.selenium.settings.TimeoutSeleniumWrapper;
+
+import junit.framework.Test;
+
 public class MainExample {
 
+	public static Test suite() {
+		SeleniumSettings settings = new SeleniumSettings(Browser.FIREFOX);
+		
+		settings.setProjectName("DemoQA - Example - Test");
+		
+		settings.setTimeoutManager(TimeoutSeleniumWrapper.DEFAULT);
+		
+		settings.setPathTestsCases("./testCases/");
+		
+		settings.setPathTestsSuites("./testSuites/");
+		
+		return FactoryMainSelenium.builder(settings);
+	}
 }
